d23df7470d88a2e1d0ad24944cf554efdcb0f651
Clematrics <clematrics.studio@gmail.com>
2019-03-24 12:04:50 +0100

    Basic SFML and ImGui
    Ability to display a window with the Dear ImGui demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+
+#include "imgui.h"
+#include "imgui-SFML.h"
+
 int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+    bool running = true;
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "RayMarching Editor");
+    ImGui::SFML::Init(window);
+
+    sf::Clock delta;
+    while (running) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+            running = event.type != event.Closed;
+        }
+
+        window.clear();
+        ImGui::SFML::Update(window, delta.restart());
+        ImGui::ShowDemoWindow();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+    window.close();
+
+    return 0;
 }
