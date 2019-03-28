903a7aed49b7349aff8d6c41cdef583e5127c98a
Clematrics <clematrics.studio@gmail.com>
2019-03-28 21:35:23 +0100

    Quick sprite rendering

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,15 @@
 #include "application_log.hpp"
 #include "configuration_loader.hpp"
 
+std::string fragShader {
+	"#version 330\n"
+	"uniform vec2 iResolution;"
+	"void main() {"
+	"	vec2 uv = gl_FragCoord.xy / iResolution.xy;"
+	"	gl_FragColor = vec4(uv, 0.3, 1.);"
+	"}"
+};
+
 int main() {
 	bool running = true;
 	sf::RenderWindow window(sf::VideoMode(1920, 1080), "RayMarching Editor");
@@ -30,11 +39,21 @@ int main() {
 		}
 
 		window.clear();
+
+		sf::Texture texture;
+		texture.create(window.getSize().x, window.getSize().y);
+		sf::Sprite sprite(texture);
+		sf::Shader shader;
+		shader.loadFromMemory(fragShader, sf::Shader::Fragment);
+		shader.setUniform("iResolution", sf::Glsl::Vec2(window.getSize().x, window.getSize().y));
+
+		window.draw(sprite, &shader);
+
 		ImGui::SFML::Update(window, delta.restart());
 		ImGui::ShowDemoWindow();
 		showDebugConsole();
-
 		ImGui::SFML::Render(window);
+
 		window.display();
 	}
 
